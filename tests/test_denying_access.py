63e930e34b93e5760385128470eae34d15aa950b
jeffr <jg725@njit.edu>
2022-05-11 21:11:58 -0400

    password confirm test

--- tests/test_denying_access.py
+++ tests/test_denying_access.py
@@ -1,7 +1,7 @@
 """This tests denying access to users NOT logged in """
 
 def test_deny_access(bad_user):
-    assert bad_user.email != 'example@email.com'
-    assert bad_user.password == 'Password1'
+    assert bad_user.email == 'jeff101@email.com'
+    assert bad_user.password != 'Jeffrey'
     response = bad_user.get("/")
     assert response.status_code == 302
