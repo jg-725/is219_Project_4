a5b9c82a31a2ac5b1884285c5c3636d91e2d5d0a
jeffr <jg725@njit.edu>
2022-05-11 20:33:20 -0400

    bad login email test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,52 +1,45 @@
 """This makes the test configuration setup"""
 # pylint: disable=redefined-outer-name
-
+import os
 import pytest
+
 from app import create_app
 from app.auth import login_form
 from app.auth import register_form
 
+
 @pytest.fixture()
-def application():
+def app():
     """This makes the app"""
-    application = create_app()
-    application.config.update({
+    #os.environ['FLASK_ENV'] = 'testing'
+    app = create_app()
+    app.config['WTF_CSRF_ENABLED'] = False
+    app.config.update({
         "TESTING": True,
     })
-    yield application
+
+    yield app
 
 
 @pytest.fixture()
-def client(application):
+def client(app):
     """This makes the http client"""
-    return application.test_client()
+    return app.test_client()
 
 
 @pytest.fixture()
-def runner(application):
+def runner(app):
     """This makes the task runner"""
-    return application.test_cli_runner()
+    return app.test_cli_runner()
 
 
 @pytest.fixture()
 def new_user():
-    user = login_form('example@email.com', 'Password')
+    user = login_form('jeff@email.com', 'Jeffrey')
     return user
 
 
 @pytest.fixture()
 def bad_user():
-    user = login_form('example@email.com', 'Password1')
-    return user
-
-
-@pytest.fixture()
-def successful_registration():
-    user = register_form('example@email.com', 'Password', 'Password')
-    return user
-
-
-@pytest.fixture()
-def bad_email_register():
-    user = register_form('example123@email.com', 'Password', 'Password')
+    user = login_form('jeff@email.com', 'Password1')
     return user
