3f3114275065b1fd48731a26fa53a455a4d1767c
jeffr <jg725@njit.edu>
2022-05-12 00:10:52 -0400

    csv file fix

--- app/csv_files_upload/__init__.py
+++ app/csv_files_upload/__init__.py
@@ -11,31 +11,32 @@ from app.db import db
 from werkzeug.utils import secure_filename, redirect
 from flask import Response
 
-map = Blueprint('map', __name__,
+csv_files_upload = Blueprint('csv_files_upload', __name__,
                         template_folder='templates')
 
 
 
-@map.route('/locations/upload', methods=['POST', 'GET'])
+@csv_files_upload.route('/upload', methods=['POST', 'GET'])
 @login_required
-def location_upload():
+def file_upload():
     form = csv_upload()
     if form.validate_on_submit():
         filename = secure_filename(form.file.data.filename)
         filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
         form.file.data.save(filepath)
-        list_of_locations = []
         with open(filepath) as file:
             csv_file = csv.DictReader(file)
+            header = next(csv_file)
+            print(header)
+            rows = []
             for row in csv_file:
-                location = Location.query.filter_by(title=row['location']).first()
-                if location is None:
-                    current_user.locations.append(Location(row['location'],row['longitude'],row['latitude'],row['population']))
+                rows.append(row)
+                if row is None:
                     db.session.commit()
                 else:
-                    current_user.locations.append(location)
+                    current_user.rows.append(row)
                     db.session.commit()
-        return redirect(url_for('map.browse_locations'))
+        return redirect(url_for('csv_files_upload.browse_locations'))
 
     try:
         return render_template('upload_locations.html', form=form)
