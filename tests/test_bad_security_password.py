c63448513502fcbbb57162634440830c0ceb99c8
jeffr <jg725@njit.edu>
2022-05-11 23:08:29 -0400

    bad password security form test

--- tests/test_bad_security_password.py
+++ tests/test_bad_security_password.py
@@ -1,10 +1,10 @@
-""" Tests the security form success """
+""" Tests the security form password failure """
 
 
 def test_security_edit_success(security_user):
     assert security_user.email == 'john@email.com'
     assert security_user.password == 'Lennon'
-    assert security_user.confirm == 'Lennon'
+    assert security_user.confirm != 'Harrison'
     response = security_user.get("/browse_users")
     assert response.status_code == 302
-    assert b'You Successfully Updated your Password or Email' in response.data
+    assert b'Passwords must match' in response.data
